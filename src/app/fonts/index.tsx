83a1e214c6f2cdecc2ab756ca8eb74c9e6352d57
Nazareno Gutierrez <nazarenojunin@gmail.com>
2025-06-03 23:00:54 -0300

    Cleaned project and added init types

---
+++ src/app/fonts/index.tsx
@@ -0,0 +1,6 @@
+import { Geist } from 'next/font/google'
+ 
+export const mainFont = Geist({
+    weight: '400',
+    subsets: ['latin'],
+})
---
+++ src/app/types/types.d.ts
@@ -0,0 +1,11 @@
+export interface ICrypto {
+  name: string;
+  ticker: string;
+  price: number;
+  amount: number;
+}
+
+export interface IUser {
+  username: string;
+  password: string;
+}
