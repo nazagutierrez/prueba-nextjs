569cf58ecc8a68d08769aaae1ef87dad11456a03
Nazareno Gutierrez <nazarenojunin@gmail.com>
2025-06-05 19:56:48 -0300

    Formatted code

--- src/app/fonts/index.tsx
+++ src/app/fonts/index.tsx
@@ -1,11 +1,11 @@
-import { Geist, Forum } from 'next/font/google'
- 
+import { Geist, Forum } from "next/font/google";
+
 export const mainFont = Geist({
-    weight: '400',
-    subsets: ['latin'],
-})
+  weight: "400",
+  subsets: ["latin"],
+});
 
 export const titleFont = Forum({
-    weight: '400',
-    subsets: ['latin'],
-})
+  weight: "400",
+  subsets: ["latin"],
+});
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-import HomePage from "@/app/(pages)/home"
+import HomePage from "@/app/(pages)/home";
 
 export default function Home() {
-  return (
-    <HomePage />
-  );
+  return <HomePage />;
 }
