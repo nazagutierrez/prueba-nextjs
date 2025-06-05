29739dd786e07f7c8ed5e40a3e96efc4dfa8ec2c
Nazareno Gutierrez <nazarenojunin@gmail.com>
2025-06-05 19:57:02 -0300

    Animations

--- src/app/utils/Skeleton.tsx
+++ src/app/utils/Skeleton.tsx
@@ -9,24 +9,22 @@ const Skeleton = () => {
           <CgSpinner className="animate-spin absolute inset-0 mx-auto h-full text-yellow-400/80 text-2xl" />
         </td>
       </tr>
-      {
-        Array.from({ length: 4 }, (_, i) => (
-          <tr key={i} className="border-b border-neutral-700/70">
-            <th scope="col" className="px-6 py-4">
-              <div className="h-5 w-20 bg-neutral-600/10 rounded-xs"></div>
-            </th>
-            <th scope="col" className="px-6 py-4">
-              <div className="h-5 w-13 bg-neutral-600/10 justify-self-center rounded-xs"></div>
-            </th>
-            <th scope="col" className="px-6 py-4">
-              <div className="h-5 w-20 bg-neutral-600/10 justify-self-center rounded-xs"></div>
-            </th>
-            <th scope="col" className="px-6 py-4">
-              <div className="h-5 w-13 bg-neutral-600/10 justify-self-center rounded-xs"></div>
-            </th>
-          </tr>
-        ))
-      }
+      {Array.from({ length: 4 }, (_, i) => (
+        <tr key={i} className="border-b border-neutral-700/70">
+          <th scope="col" className="px-6 py-4">
+            <div className="h-5 w-20 bg-neutral-600/10 animate-pulse rounded-xs"></div>
+          </th>
+          <th scope="col" className="px-6 py-4">
+            <div className="h-5 w-13 bg-neutral-600/10 animate-pulse justify-self-center rounded-xs"></div>
+          </th>
+          <th scope="col" className="px-6 py-4">
+            <div className="h-5 w-20 bg-neutral-600/10 animate-pulse justify-self-center rounded-xs"></div>
+          </th>
+          <th scope="col" className="px-6 py-4">
+            <div className="h-5 w-13 bg-neutral-600/10 animate-pulse justify-self-center rounded-xs"></div>
+          </th>
+        </tr>
+      ))}
     </thead>
   );
 };
