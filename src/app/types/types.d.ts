72312849d02ce4426c091daedca7128162838b36
Nazareno Gutierrez <nazarenojunin@gmail.com>
2025-06-05 19:57:27 -0300

    Added more types

--- src/app/types/types.d.ts
+++ src/app/types/types.d.ts
@@ -1,7 +1,32 @@
+import { Dispatch, SetStateAction } from "react";
+
 export interface ICrypto {
   id: string;
   name: string;
   ticker: string;
   price: number;
   amount: number;
+}
+
+export interface IUserContext {
+  user: IUser;
+  setUser: Dispatch<SetStateAction<IUser>>;
+}
+
+export interface IUser {
+  username: string;
+  password: string;
+}
+
+export interface ModalProps {
+  open: boolean;
+  crypto: ICrypto;
+  onClose: () => void;
+}
+export interface FormProps {
+  name: string;
+  type: string;
+  classname?: string;
+  children?: React.ReactNode;
+  submitFunc?: () => void;
 }
