b2b4e35af75e91143ef5d653f66cc5a98ad670a2
Nazareno Gutierrez <nazarenojunin@gmail.com>
2025-06-04 21:31:11 -0300

    Home and Navbar

--- src/app/(pages)/home/index.tsx
+++ src/app/(pages)/home/index.tsx
@@ -1,16 +1,23 @@
+import { titleFont } from '@/app/fonts'
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
 const HomePage = () => {
   return (
-    <div className='flex flex-col items-center justify-center h-screen'>
-      <h1 className='text-5xl mb-4'>Cypto market viewer</h1>
-      <h2 className='mb-20'>Check the entire crypto market in one site</h2> 
-      <div className='flex gap-4'>
-        <Link href="/auth" className="bg-white px-2 rounded py-0.5 text-black">Login</Link>
-        <Link href="/crypto" className="bg-white px-2 rounded py-0.5 text-black">Get started</Link>
+    <>
+      <div className='flex flex-col items-center justify-center pt-52 h-[70vh]'>
+        <div className='absolute top-1/4 right-60 h-32 w-32 bg-white/20 blur-3xl'></div>
+        <h1 className={`${titleFont.className} text-6xl mb-4`}>Crypto market viewer</h1> 
+        <h2 className='mb-20'>Check the <span className='underline underline-offset-4 decoration-yellow-500/80'>entire</span> crypto market in one list</h2> 
+        <Link href="/crypto" className="bg-white/80 px-3 rounded py-1 text-black">Get started</Link>
       </div>
-    </div>
+      <div className='relative flex flex-col items-center justify-center h-screen'>
+        <Image src="/image.avif" alt="crypto market" className='border border-t-neutral-600 border-neutral-800/90 rounded-sm overflow-hidden z-20' width={1000} height={500} />
+        <div className='absolute mx-auto h-[630px] w-[350px] bg-white/40 blur-3xl'></div>
+        <h2 className='mt-5'>Preview of the tool</h2> 
+      </div>
+    </>
   )
 }
 
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -6,11 +6,11 @@ import { MdHomeFilled } from "react-icons/md";
 
 const Navbar = () => {
   return (
-    <div className="fixed text-white-main w-full flex items-center justify-center">
-      <div className="w-fit mt-3 h-16 bg-neutral-800 rounded-full flex items-center justify-center">
+    <div className="fixed text-white-main w-full flex items-center justify-center z-20">
+      <div className="w-fit mt-3 h-14 bg-neutral-800 rounded-full flex items-center justify-center">
         <Link
           href="/"
-          className="group relative bg-neutral-600/70 gap-2 w-[80px] h-[80%] px-4 content-center justify-items-center text-center rounded-l-full m-2 transition-all duration-300 ease-in-out"
+          className="group relative bg-neutral-600/70 hover:bg-neutral-800 border border-neutral-600/70 gap-2 w-[80px] h-[80%] px-4 content-center justify-items-center text-center rounded-l-full m-2 transition-all duration-300 ease-in-out"
         >
           <MdHomeFilled className="text-xl opacity-0 translate-y-0 group-hover:opacity-100 group-hover:translate-y-0 transition-all duration-300" />
           <h2 className="absolute h-full inset-0 content-center opacity-100 group-hover:opacity-0 group-hover:translate-y-2 transition-all duration-300">
@@ -19,16 +19,16 @@ const Navbar = () => {
         </Link>
         <Link
           href="/crypto"
-          className="relative group bg-neutral-600/70 gap-2 w-[150px] h-[80%] px-4 content-center justify-items-center text-center rounded-sm m-2"
+          className="relative group bg-neutral-600/70 hover:bg-neutral-800 border border-neutral-600/70 gap-2 w-[150px] h-[80%] px-4 content-center justify-items-center text-center rounded-sm m-2 transition-all duration-300 ease-in-out"
         >
-          <BsGraphUp className="text-xl opacity-0 translate-y-0 group-hover:opacity-100 group-hover:translate-y-0 transition-all duration-300" />
+          <BsGraphUp className="text-lg opacity-0 translate-y-0 group-hover:opacity-100 group-hover:translate-y-0 transition-all duration-300" />
           <h2 className="absolute h-full inset-0 content-center opacity-100 group-hover:opacity-0 group-hover:translate-y-2 transition-all duration-300">
             Crypto viewer
           </h2>
         </Link>
         <Link
           href="/auth"
-          className="relative group bg-neutral-600/70 gap-2 w-[80px] h-[80%] px-4 content-center justify-items-center text-center rounded-r-full m-2"
+          className="relative group bg-neutral-600/70 hover:bg-neutral-800 border border-neutral-600/70 gap-2 w-[80px] h-[80%] content-center justify-items-center text-center rounded-r-full m-2 transition-all duration-300 ease-in-out"
         >
           <IoLogInOutline className="text-xl opacity-0 translate-y-0 group-hover:opacity-100 group-hover:translate-y-0 transition-all duration-300" />
           <h2 className="absolute h-full inset-0 content-center opacity-100 group-hover:opacity-0 group-hover:translate-y-2 transition-all duration-300">
