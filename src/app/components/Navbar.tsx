380f122005c1238c2ba70e1e1094300b05108373
Nazareno Gutierrez <nazarenojunin@gmail.com>
2025-06-04 23:43:16 -0300

    Little navbar changes

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -10,27 +10,27 @@ const Navbar = () => {
       <div className="w-fit mt-3 h-14 bg-neutral-800 rounded-full flex items-center justify-center">
         <Link
           href="/"
-          className="group relative bg-neutral-600/70 hover:bg-neutral-800 border border-neutral-600/70 gap-2 w-[80px] h-[80%] px-4 content-center justify-items-center text-center rounded-l-full m-2 transition-all duration-300 ease-in-out"
+          className="group relative bg-neutral-600/50 hover:bg-neutral-800 border border-yellow-600/20 gap-2 w-[80px] h-[80%] px-4 content-center justify-items-center text-center rounded-l-full m-2 transition-all duration-300 ease-in-out"
         >
-          <MdHomeFilled className="text-xl opacity-0 translate-y-0 group-hover:opacity-100 group-hover:translate-y-0 transition-all duration-300" />
+          <MdHomeFilled className="text-xl text-yellow-100 opacity-0 translate-y-0 group-hover:opacity-100 group-hover:translate-y-0 transition-all duration-300" />
           <h2 className="absolute h-full inset-0 content-center opacity-100 group-hover:opacity-0 group-hover:translate-y-2 transition-all duration-300">
             Home
           </h2>
         </Link>
         <Link
           href="/crypto"
-          className="relative group bg-neutral-600/70 hover:bg-neutral-800 border border-neutral-600/70 gap-2 w-[150px] h-[80%] px-4 content-center justify-items-center text-center rounded-sm m-2 transition-all duration-300 ease-in-out"
+          className="relative group bg-neutral-600/50 hover:bg-neutral-800 border border-yellow-600/20 gap-2 w-[150px] h-[80%] px-4 content-center justify-items-center text-center rounded-sm m-2 transition-all duration-300 ease-in-out"
         >
-          <BsGraphUp className="text-lg opacity-0 translate-y-0 group-hover:opacity-100 group-hover:translate-y-0 transition-all duration-300" />
+          <BsGraphUp className="text-lg text-yellow-100 opacity-0 translate-y-0 group-hover:opacity-100 group-hover:translate-y-0 transition-all duration-300" />
           <h2 className="absolute h-full inset-0 content-center opacity-100 group-hover:opacity-0 group-hover:translate-y-2 transition-all duration-300">
             Crypto viewer
           </h2>
         </Link>
         <Link
           href="/auth"
-          className="relative group bg-neutral-600/70 hover:bg-neutral-800 border border-neutral-600/70 gap-2 w-[80px] h-[80%] content-center justify-items-center text-center rounded-r-full m-2 transition-all duration-300 ease-in-out"
+          className="relative group bg-neutral-600/50 hover:bg-neutral-800 border border-yellow-600/20 gap-2 w-[80px] h-[80%] content-center justify-items-center text-center rounded-r-full m-2 transition-all duration-300 ease-in-out"
         >
-          <IoLogInOutline className="text-xl opacity-0 translate-y-0 group-hover:opacity-100 group-hover:translate-y-0 transition-all duration-300" />
+          <IoLogInOutline className="text-xl text-yellow-100 opacity-0 translate-y-0 group-hover:opacity-100 group-hover:translate-y-0 transition-all duration-300" />
           <h2 className="absolute h-full inset-0 content-center opacity-100 group-hover:opacity-0 group-hover:translate-y-2 transition-all duration-300">
             Login
           </h2>
